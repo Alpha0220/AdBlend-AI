326b0b4031c3ca94a7255276af26efc1897b63e7
Alpha0220 <thanakritskyheart@gmail.com>
2025-11-14 16:03:49 +0700

    change logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import AdBlendForm from '@/components/ad-blend-form';
-import { Blend } from 'lucide-react';
+import AdBlendForm from "@/components/ad-blend-form";
+import limitlessLogo from "@/public/assets/limitless-logo.jpg";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-14 items-center">
+          <div className="container flex h-14 items-center">
           <div className="mr-4 flex items-center">
-            <Blend className="h-6 w-6 mr-2 text-primary" />
+              <div className="ml-12 mr-4 p-1 md:p-2 ">
+                <Image
+                  src={limitlessLogo}
+                  alt="Limitless Studio logo"
+                  width={48}
+                  height={48}
+                  className="rounded-full"
+                  priority
+                />
+              </div>
+            {/* <Blend className="h-6 w-6 mr-2 text-primary" /> */}
             <h1 className="text-xl font-bold tracking-tight font-headline">
-              AdBlend AI
+              Limitless Studio
             </h1>
           </div>
         </div>
@@ -20,7 +31,7 @@ export default function Home() {
       <footer className="py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground">
-            Built with Next.js and Firebase Genkit.
+            Every image you dream of, made real here.
           </p>
         </div>
       </footer>
